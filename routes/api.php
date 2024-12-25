b177bc24b1a62c2f316b73c385978242b28704af
Haidar Mohammad <it.eng.haidar@gmail.com>
2024-12-25 16:41:29 +0300

    api-v1

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1')->group(function () {
 
-include __DIR__ . "/v1/Mobile/AuthMobile.php";
-include __DIR__ . "/v1/Mobile/Profile.php";
-include __DIR__ . "/v1/Mobile/Driver.php";
-
-
-Route::get("/image", function (Request $request) {
-
-    
-    // continue to test optimize image 
-
-
+    include __DIR__ . "/v1/Mobile/AuthMobile.php";
+    include __DIR__ . "/v1/Mobile/Profile.php";
+    include __DIR__ . "/v1/Mobile/Driver.php";
 });
+
